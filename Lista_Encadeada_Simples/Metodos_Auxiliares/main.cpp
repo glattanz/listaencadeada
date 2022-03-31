5c265932f44e18a495e7839c2294663b0d4ffbf9
Gabrielle Lattanzi <82163272+glattanz@users.noreply.github.com>
2022-03-31 17:23:15 -0300

    Update main.cpp

--- Lista_Encadeada_Simples/Metodos_Auxiliares/main.cpp
+++ Lista_Encadeada_Simples/Metodos_Auxiliares/main.cpp
@@ -5,15 +5,15 @@
 
 int menu(){
 	
-	printf("1 - Mostrar início\n");
+	printf("1 - Mostrar inÃ­cio\n");
 	printf("2 - Mostrar fim\n");
 	printf("3 - Informar o tamanho da lista\n");
-	printf("4 - Informar o número de ocorrências de um elemento\n");
+	printf("4 - Informar o nÃºmero de ocorrÃªncias de um elemento\n");
 	printf("5 - Remove elemento do inicio da lista\n");
 	printf("6 - Remove elemento do fim da lista\n");
 	printf("7 - Inserir no fim\n");
 	
-	printf("Insira a opção desejada: ");
+	printf("Insira a opÃ§Ã£o desejada: ");
 	scanf("%d", &op);
 	return op;
 }
@@ -26,7 +26,7 @@ int main(int argc, char** argv) {
 	int x; //auxiliar
 	int op;
 	
-	//l é uma variavel tipo struct (Lista) q representa um no de dado
+	//l Ã© uma variavel tipo struct (Lista) q representa um no de dado
 	l = inicializa();
 	
 	do{
@@ -36,15 +36,15 @@ int main(int argc, char** argv) {
 		switch(op)
 		{
 			case 1:
-				
+				mmostraInicio(Lista* l);
 			
 			case 2:
-				
+				mostraFim(Lista* l);
 
 			case 3: 
 				int t = tamanho(Lista* l);
 				
-				printf("O tamanho da lista é de %d elementos.", t);
+				printf("O tamanho da lista Ã© de %d elementos.", t);
 			
 			case 4:
 				printf("Digite um elemento para saber quantas vezes ele aparece na lista:");
@@ -62,8 +62,11 @@ int main(int argc, char** argv) {
 				
 				
 			case 6:
-				
-				
+				removeFim()
+				printf("Elemento removido!");
+ 				
+ 				printf("Lista atual:");
+ 				imprime(Lista* l);
 				
 			case 7:
 				printf("Informe o valor a ser inserido:")
@@ -72,7 +75,7 @@ int main(int argc, char** argv) {
 				l = insereFim(l, x);
 				
 			default:
-				printf("\nInsira uma das opções acima!"\n);
+				printf("\nInsira uma das opÃ§Ãµes acima!"\n);
 				
 		}
 	}while(op > 0 && op < 8);
